fd299dc6dcdcff73f87a7f43136d6a9d690ef3a9
Isamu Ito <isamsito@gmail.com>
2021-02-11 23:58:09 +0000

    implemented longitude latitude using ip

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const request = require('request');
+
+
+
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -6,5 +10,12 @@ fetchMyIP((error, ip) => {
     return;
   }
   console.log('It worked! Returned IP:' , ip);
+  fetchCoordsByIP(ip,(error, location) => {
+    if (error) {
+      console.log("It didn't work!" , error);
+      return;
+    }
+    console.log('It worked! Returned coordinates:' , location);
+  })
 });
 
--- iss.js
+++ iss.js
@@ -25,5 +25,22 @@ const fetchMyIP = function(callback) {
   });
 };
 
+const fetchCoordsByIP = (ip, callback) => {
+  request(`https://freegeoip.app/json/${ip}`,(error,response,body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const {latitude, longitude} =JSON.parse(body);
+    callback(null, myLocation);
+});
+}
 
-module.exports = { fetchMyIP };
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
